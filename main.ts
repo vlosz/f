be491f43d1e96faf29b8f6b15add9523eb625dae
vlosz <156816464+vlosz@users.noreply.github.com>
2024-01-22 09:21:16 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,12 @@
+basic.forever(function () {
+    if (input.temperature() < 20) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+    } else {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+    }
+})
+basic.forever(function () {
+    if (0 == 0) {
+    	
+    }
+})
